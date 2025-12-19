cbebe6033e792d045a93d2d7eb121d18cab6bc7c
H4x06KH <mattia.lotto.2005@gmail.com>
2025-12-19 13:43:18 +0100

    git_5 aggiornata

--- Assets/Course Library/Scripts/GameManager.cs
+++ Assets/Course Library/Scripts/GameManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    public TextMeshProUGUI scoreText;
     public GameObject[] targets;
     public float spawnInterval = 1.0f;
+    private int score = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+       
 
     }
     IEnumerator SpawnTarget()
@@ -20,13 +24,14 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnInterval);
             int index = Random.Range(0,targets.Length);
             Instantiate(targets[index]);
-
+            UpdateScore(5);
         }
     }
 
     // Update is called once per frame
-    void Update()
+    private void UpdateScore(int scoreToAdd)
     {
-        
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
